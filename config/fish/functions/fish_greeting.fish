93a02d10238b09b4b0539a01f84cc4830f9fc8a4
june <me@koi.rip>
2026-08-01 18:10:01 +0200

    fish: print motd in greeting

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,4 +1,9 @@
 function fish_greeting
+  and set_color $fish_color_comment
+  test -f /etc/motd
+  and cat /etc/motd
+  and set_color $fish_color_normal
+
   set -l path "$XDG_DOCUMENTS_DIR/todo.md"
   test -f "$path"
   and set_color $fish_color_operator
